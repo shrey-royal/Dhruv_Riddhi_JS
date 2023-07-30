434b3ba884b4b51f71b056ad161b4a1eadfa2b97
Shrey K <srkkadia@gmail.com>
2023-07-30 10:49:50 +0530

    Fetch data using API

--- HTTP_JS/script.js
+++ HTTP_JS/script.js
@@ -1,26 +1,56 @@
-function loadDoc() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200){
-            document.getElementById("demo").innerHTML = this.responseText;
-        }
-    };
-    xhttp.open("GET", "intro.txt", true);   // open(method, url, async)
-    xhttp.send();   // send() method sends a request to the server (used for GET)
+// function getData() {
+//     var xhr = new XMLHttpRequest();
+
+//     //configure the request (GET method, URL to the data)
+//     xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts', true);
+
+//     //set up a function to handle the response
+//     xhr.onload = function () {
+//         if(xhr.status >= 200 && xhr.status < 300) {
+//             //Request was successful
+//             var responseData = JSON.parse(xhr.responseText);
+
+//             //Update the data container with the retrieved data
+//             var dataContainer = document.getElementById('data-container');
+//             dataContainer.innerHTML = JSON.stringify(responseData, null, 2);
+//         } else {
+//             //Request failed with an error
+//             console.error('Request failed with error: ' + xhr.status);
+//         }
+//     };
+
+//     // set up a function to handle errors
+//     xhr.onerror = function () {
+//         console.error("Request Failed!!");
+//     };
+
+//     //send the request
+//     xhr.send();
+// }
+
+function getData() {    //fetch() returns a promise
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => { //this is to check if the response is ok
+            if(!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            //Update the data container with the retrieved data
+            var dataContainer = document.getElementById('data-container');
+            dataContainer.innerHTML = JSON.stringify(data, null, 2);
+        })
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation: ', error);
+        });
 }
 
-// onreadystatechange: Defines a function to be called when the readyState property changes
+getData();
 
+//JSON.stringify() converts a JavaScript object or value to a JSON string
 
-const x = document.getElementById("getLoc");
-function getLocation() {
-    if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
-    }
-}
-
-function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;    
-}
+/*
+syntax:
+JSON.stringify(value, replacer, space)
+*/
