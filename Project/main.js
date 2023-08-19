51b4a4c18514068c729ea0ccfc0419197c51da81
Shrey K <srkkadia@gmail.com>
2023-08-19 13:45:21 +0530

    Project Finish

--- Project/main.js
+++ Project/main.js
@@ -14,18 +14,18 @@ const btn = document.querySelector('.submit')
 const cities = document.querySelectorAll('.city')
 
 //Default city when page loads
-let cityInput = 'Ahmedabad'
+let cityInput = 'ahmedabad'
 
 //Add click event to each city in the panel
 cities.forEach((city) => {
     city.addEventListener('click', (e) => {
         //Change from default city to the clicked one
-        cityInput = e.target.innerHTMl;
+        cityInput = e.target.innerHTML; // Corrected typo
         //Call the function to get the weather data
         fetchWeatherData();
         app.style.opacity = "0";
     });
-})
+});
 
 form.addEventListener('submit', (e) => {
     if(search.value.length == 0) {
@@ -33,25 +33,34 @@ form.addEventListener('submit', (e) => {
     } else {
         cityInput = search.value;
         fetchWeatherData();
-        console.log(cityInput);
+        // console.log(cityInput);
         search.value = "";
         app.style.opacity = "0";
     }
 
-    e.preventDefault();
+    e.preventDefault(); //it will prevent the page from reloading
 });
 
 function dayOfTheWeek(day, month, year) {
     const Weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-    return Weekdays[new Date(`${day}/${month}/${year}`).getDay()];
-};
+    // Create a new Date object with the provided year, month (0-based), and day.
+    const date = new Date(year, month - 1, day);
+
+    // Check if the date is valid before getting the day of the week.
+    if (isNaN(date)) {
+        return "Invalid date";
+    }
+
+    return Weekdays[date.getDay()];
+}
+
 
 function fetchWeatherData() {
     fetch(`https://api.weatherapi.com/v1/current.json?key=e26ee2f87b994c98863100552231608&q=${cityInput}`)
         .then(response => response.json())
         .then(data => {
-            console.log(data);
+            // console.log(data);
 
             temp.innerHTML = data.current.temp_c + "&#176;";
             conditionOutput.innerHTML = data.current.condition.text;
@@ -81,7 +90,7 @@ function fetchWeatherData() {
 
             const code = data.current.condition.code;   //1000
 
-            if(!data.cuurent.is_day) {
+            if(!data.current.is_day) {
                 timeOfDay = "night";
             }
 
@@ -104,13 +113,53 @@ function fetchWeatherData() {
                 code == 1279 ||
                 code == 1282
             ) {
-                app.style.background = `url(./images/${timeOfDay}/cloudy.jpg)`;
+                app.style.backgroundImage = `url(./images/${timeOfDay}/cloudy.jpg)`;
                 btn.style.background = "#faf6d1b";
                 if(timeOfDay == "night") {
                     btn.style.background = "#181e27";
                 }
+            // rain
+            } else if (
+                code == 1063 ||
+                code == 1069 ||
+                code == 1072 ||
+                code == 1150 ||
+                code == 1153 ||
+                code == 1180 ||
+                code == 1183 ||
+                code == 1186 ||
+                code == 1189 ||
+                code == 1192 ||
+                code == 1195 ||
+                code == 1204 ||
+                code == 1207 ||
+                code == 1240 ||
+                code == 1243 ||
+                code == 1246 ||
+                code == 1249 ||
+                code == 1252
+            ) {
+                app.style.backgroundImage = `url(./images/${timeOfDay}/rainy.jpg)`;
+                btn.style.background = "#647d75";
+
+                if(timeOfDay == "night") {
+                    btn.style.background = "#325c80";
+                }
+            } else {
+                app.style.backgroundImage = `url(./images/${timeOfDay}/snowy.jpg)`;
+                btn.style.background = "#4d72aa";
+                if(timeOfDay == "night") {
+                    btn.style.background = "#1b1b1b";
+                }
             }
 
-
+            app.style.opacity = "1";
+        }).catch(() => {
+            alert("City not found, please try again");
+            app.style.opacity = "1";
         });
 }
+
+fetchWeatherData();
+
+app.style.opacity = "1";
